d224310eae63ff27489f5e24bc4fe1f31b22fcfd
Julie Kim <juliekimjiehyun@gmail.com>
2023-05-20 02:48:54 -0700

    Async DisplayList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route} from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import { AboutPage, CampsiteDetailPage, CampsitesDirectoryPage, ContactPage, HomePage} from './pages';
 import { Header, Footer } from './components';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+        dispatch(fetchPartners());
+        dispatch(fetchPromotions());
+    }, [dispatch]);
+
   return (
     <div className="App">
         <Header/>
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,9 +1,11 @@
 import { useSelector } from 'react-redux';
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+import DisplayCard from './DisplayCard'
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice'; 
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const DisplayList = () => {
     const items = useSelector((state) => [
@@ -13,13 +15,23 @@ const DisplayList = () => {
     ]);
 
     console.log('display items:', items);
+
     return (
         <Row>
             {items.map((item, idx) => {
+                const { featuredItem, isLoading, errMsg } = item;
+                if (isLoading) {
+                    return <Loading key={idx} />;
+                }
+                if (errMsg) {
+                    return <Error errMsg={errMsg} key={idx} />;
+                }
                 return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
+                    featuredItem && (
+                        <Col md className='m-1' key={idx}>
+                            <DisplayCard item={featuredItem} />
+                        </Col>
+                    )
                 );
             })}
         </Row>
